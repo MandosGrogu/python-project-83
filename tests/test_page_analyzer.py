33a3c7681708684007f95aa269d1bceabde2e221
MandosGrogu <jpinkmann@mail.ru>
2026-07-22 21:52:58 +0300

    union two test files

--- tests/test_page_analyzer.py
+++ tests/test_page_analyzer.py
@@ -1,4 +1,15 @@
+import pytest
+from page_analyzer.app import app
 
+
+@pytest.fixture()
+def client():
+
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+    
 def test_index_route(client):
 
     response = client.get("/")
