aca064c146b280c6b664fd062c86d0eb71d7d507
MandosGrogu <jpinkmann@mail.ru>
2026-09-13 18:05:40 +0300

    fix errors marked by linter test

--- tests/test_page_analyzer.py
+++ tests/test_page_analyzer.py
@@ -17,8 +17,7 @@ def test_index_route(client):
     response = client.get("/")
     html = response.data.decode()
     assert response.status_code == 200
-    assert '''<label for="basic-url" class="form-label">
-    Бесплатно проверяйте сайты на SEO-пригодность</label>''' in html
+    assert 'Бесплатно проверяйте сайты на SEO-пригодность' in html
 
 
 def test_get_url_route(client):
@@ -32,8 +31,7 @@ def test_get_urls_route(client):
     response = client.get("/urls")
     html = response.data.decode()
     assert response.status_code == 200
-    assert '''<table class="table table-bordered table-hover text-nowrap"
-     data-test="urls">''' in html
+    assert 'data-test="urls"' in html
 
 
 def test_post_check_route(client):
